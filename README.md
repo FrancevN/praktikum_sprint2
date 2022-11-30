0c6f63db71729e808fef85882817a3efa8263fd6
FrancevN <francevnik@icloud.com>
2022-11-30 04:53:26 +0400

    done tests

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# qa_python
+# Реализованные тесты:
+
+1. test_add_new_book_add_two_books - проверка добавления двух книг
+2. test_add_new_book_add_book - проверка добавления книги
+3. test_add_two_identical_books_add_one_book - проверка не возможности добавить две идентичные книги
+4. test_not_rate_a_book_that_is_on_the_list_is_none - проверка невозможности выставить рейтинг книге, которой нет в списке
+5. test_dont_rate_less_than_1_get_1 - проверка невозможности выставить рейтинг меньше 1
+6. test_dont_rate_more_than_10_get_1 - проверка невозможности выставить рейтинг больше 10
+7. test_book_not_added_has_no_rating_is_none - у не добавленной книги нет рейтинга
+8. test_add_book_to_favorites_add_favorites - добавление книги в избранное
+9. test_cannot_add_book_to_favorites_if_it_is_not_in_dictionary_books_rating_favorites_is_empty - нельзя добавить книгу в избранное, если её нет в словаре books_rating
+10. test_book_remove_in_favorites_favorites_is_empty - Проверка удаления книги из избранного
--- tests.py
+++ tests.py
@@ -23,3 +23,53 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    def test_add_new_book_add_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('ДРАКОНЫ')
+        assert len(collector.get_books_rating()) == 1
+
+    def test_add_two_identical_books_add_one_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('ДРАКОНЫ')
+        collector.add_new_book('ДРАКОНЫ')
+        assert len(collector.get_books_rating()) == 1
+
+    def test_not_rate_a_book_that_is_on_the_list_is_none(self):
+        collector = BooksCollector()
+        collector.add_new_book('ДРАКОНЫ')
+        assert collector.set_book_rating('Гордость и предубеждение и зомби', 5) is None
+
+    def test_dont_rate_less_than_1_get_1(self):
+        collector = BooksCollector()
+        collector.add_new_book('ДРАКОНЫ')
+        collector.set_book_rating('ДРАКОНЫ', 0)
+        assert collector.get_book_rating('ДРАКОНЫ') == 1
+
+    def test_dont_rate_more_than_10_get_1(self):
+        collector = BooksCollector()
+        collector.add_new_book('ДРАКОНЫ')
+        collector.set_book_rating('ДРАКОНЫ', 11)
+        assert collector.get_book_rating('ДРАКОНЫ') == 1
+
+    def test_book_not_added_has_no_rating_is_none(self):
+        collector = BooksCollector()
+        assert collector.get_book_rating('ДРАКОНЫ') is None
+
+    def test_add_book_to_favorites_add_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book('ДРАКОНЫ')
+        collector.add_book_in_favorites('ДРАКОНЫ')
+        assert collector.favorites == ['ДРАКОНЫ']
+
+    def test_cannot_add_book_to_favorites_if_it_is_not_in_dictionary_books_rating_favorites_is_empty(self):
+        collector = BooksCollector()
+        collector.add_book_in_favorites('ДРАКОНЫ')
+        assert collector.favorites == []
+
+    def test_book_remove_in_favorites_favorites_is_empty(self):
+        collector = BooksCollector()
+        collector.add_new_book('ДРАКОНЫ')
+        collector.add_book_in_favorites('ДРАКОНЫ')
+        collector.delete_book_from_favorites('ДРАКОНЫ')
+        assert collector.favorites == []
